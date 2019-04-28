9c5bc3ce82b09a7aada867c76170ee4a3004a3d1
sidibos <sidibemousse@hotmail.com>
2019-04-28 03:28:21 +0100

    Fix composer install issue

--- thg/scripts/run_tests.sh
+++ thg/scripts/run_tests.sh
@@ -7,14 +7,9 @@ apt-get update
 
 apt-get install -y git unzip
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --filename=composer --install-dir=/usr/bin
-php -r "unlink('composer-setup.php');"
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 
 composer install --no-progress --no-suggest
-
-#curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 
 cd prepaid-card
 
